8b43f68a1d8fdbf3916ac1d286ed9ebf618bf6ba
abhishek <asmi9574@gmail.com>
2023-12-14 17:12:30 +0530

    first

--- src/components2/Section/First.js
+++ src/components2/Section/First.js
@@ -10,72 +10,86 @@ import { MdOutlineEventNote } from "react-icons/md";
 
 const First = () => {
   return (
-    <div style={{background:'#FAD2D2'}}>
-      <h1 style={{color:'#004080',display:'flex', justifyContent:'center', alignItems:'center'}}>Connect And Collaborate</h1>
-      <div style={{display:'flex', justifyContent:'space-evenly',marginTop:'50px'}}>
-        {/* left */}
-        <div style={{background:'#FFE4E4'}}>
-            <div style={{background:'white', padding:'5%',width:'8vw',height:'28vh',borderRadius:'10px'}}>
-            <div style={{display:'flex',justifyContent:'center', alignItems:'center'}}>
-                <h5>My Groups</h5>
-            </div>
-            <div style={{display:'flex',justifyContent:'space-evenly'}}>
-                <MdGroups />
-                <MdGroups />
-            </div>
-            <div style={{display:'flex',justifyContent:'space-evenly'}}>
-                <MdGroups />
-                <MdGroups />
-            </div>
-            <div style={{marginTop:'30px'}}>
-            <div style={{display:'flex',justifyContent:'center', alignItems:'center'}}>
-                <h5>Events</h5>
-            </div>
-            <div style={{display:'flex',justifyContent:'space-evenly'}}>
-            <SlCalender />
-            <SlCalender />
-            </div>
-            <div style={{display:'flex',justifyContent:'space-evenly',marginTop:'4px'}}>
-            <SlCalender />
-            <SlCalender />
-            </div>
-            </div>
-            </div>
-        </div>
-        {/* mid */}
-        <div style={{background:'#FFE4E4',height:'20vh',width:'30vw',borderRadius:'10px'}}>
-           <div style={{display:'flex',justifyContent:'space-evenly'}}>
-            <div style={{fontSize:'3rem'}}>
-                <CgProfile />
-            </div>
-            <div style={{marginTop:'15px'}}>
-                <label htmlFor='search'></label>
-                <input id='search' placeholder='Create Post...' style={{background:'white', borderRadius:'10px',height:'8vh',width:'20vw',}}></input>
-            </div>
-           </div>
-           <div style={{display:'flex',fontSize:'2rem',justifyContent:'space-evenly'}}>
-            <span>Media <MdOutlinePermMedia/></span>        
-            <span>event <MdOutlineEventNote/></span>   
-           </div>
-        </div>
-        {/* right */}
-        <div style={{background:'#FFE4E4'}}>
-          <div style={{background:'white', padding:'5%',justifyContent:'center',alignItems:'center',width:'10vw',borderRadius:'10px'}}>
-            <h5 style={{display:'flex',justifyContent:'center', alignItems:'center'}}>Top News</h5>
-            <div style={{background:'#FFE4E4', padding:'5%',justifyContent:'center',alignItems:'center',borderRadius:'5px'}}>
-                <h6 style={{display:'flex',justifyContent:'center', alignItems:'center'}}>News 1</h6>
-                <h6 style={{display:'flex',justifyContent:'center', alignItems:'center'}}>News 2</h6>
-                <h6 style={{display:'flex',justifyContent:'center', alignItems:'center'}}>News 3</h6>
-                <h6 style={{display:'flex',justifyContent:'center', alignItems:'center'}}>News 4</h6>
-                <h6 style={{display:'flex',justifyContent:'center', alignItems:'center'}}>News 5</h6>
+    <>
+      <div style={{background:'#FAD2D2'}}>
+        <h1 style={{color:'#004080',display:'flex', justifyContent:'center', alignItems:'center',fontSize:'2.5rem'}}>Connect And Collaborate</h1>
+        <div style={{display:'flex', justifyContent:'space-evenly',marginTop:'50px'}}>
+          {/* left */}
+          <div style={{background:'#FFE4E4',borderRadius:'20px'}}>
+            <div style={{padding:'20px'}}>
+                <div style={{background:'white', padding:'10px',width:'8vw',height:'28vh',borderRadius:'10px'}}>
+                <div style={{display:'flex',justifyContent:'center', alignItems:'center'}}>
+                    <h5>My Groups</h5>
+                </div>
+                <div style={{display:'flex',justifyContent:'space-evenly'}}>
+                    <MdGroups />
+                    <MdGroups />
+                </div>
+                <div style={{display:'flex',justifyContent:'space-evenly'}}>
+                    <MdGroups />
+                    <MdGroups />
+                </div>
+                <div style={{marginTop:'30px'}}>
+                <div style={{display:'flex',justifyContent:'center', alignItems:'center'}}>
+                    <h5>Events</h5>
+                </div>
+                <div style={{display:'flex',justifyContent:'space-evenly'}}>
+                <SlCalender />
+                <SlCalender />
+                </div>
+                <div style={{display:'flex',justifyContent:'space-evenly',marginTop:'4px'}}>
+                <SlCalender />
+                <SlCalender />
+                </div>
+                </div>
+                </div>
             </div>
           </div>
+          {/* mid */}
+          <div style={{background:'#FFE4E4',height:'20vh',width:'30vw',borderRadius:'10px'}}>
+            <div style={{display:'flex',justifyContent:'space-evenly'}}>
+              <div style={{fontSize:'3rem', marginTop:'20px'}}>
+                  <CgProfile />
+              </div>
+              <div style={{marginTop:'15px'}}>
+                  <label htmlFor='search'></label>
+                  <input id='search' placeholder='Create Post...' style={{background:'white', borderRadius:'10px',height:'8vh',width:'20vw',}}></input>
+              </div>
+            </div>
+            <div style={{display:'flex',fontSize:'1.5rem',justifyContent:'space-evenly'}}>
+              <span style={{display:'flex',justifyContent:'space-evenly',gap:'10px',marginTop:'15px'}}>Media <MdOutlinePermMedia style={{marginTop:'6px'}}/></span>        
+              <span style={{display:'flex',justifyContent:'space-evenly',gap:'10px',marginTop:'15px'}}>event <MdOutlineEventNote style={{marginTop:'6px'}}/></span>   
+            </div>
+          </div>
+          {/* right */}
+          <div style={{background:'#FFE4E4',padding:'15px',height:'35vh',borderRadius:'20px'}}>
+              <div style={{background:'white', padding:'5%',justifyContent:'center',alignItems:'center',width:'10vw',height:'30vh',borderRadius:'20px'}}>
+                <h5 style={{display:'flex',justifyContent:'center', alignItems:'center'}}>Top News</h5>
+                <div style={{background:'#FFE4E4', padding:'5%',justifyContent:'center',alignItems:'center',borderRadius:'10px'}}>
+                  <div style={{border:'2px solid #ccc',borderTop:'none'}}>
+                    <h6 style={{display:'flex',justifyContent:'center', alignItems:'center',margin:'5px 0'}}>News 1</h6>
+                  </div>
+                  <div style={{border:'2px solid #ccc',borderTop:'none'}}>
+                    <h6 style={{display:'flex',justifyContent:'center', alignItems:'center',margin:'5px 0'}}>News 2</h6>
+                  </div>
+                  <div style={{border:'2px solid #ccc',borderTop:'none'}}>
+                    <h6 style={{display:'flex',justifyContent:'center', alignItems:'center',margin:'5px 0'}}>News 3</h6>
+                  </div>
+                  <div style={{border:'2px solid #ccc',borderTop:'none'}}>
+                    <h6 style={{display:'flex',justifyContent:'center', alignItems:'center',margin:'5px 0'}}>News 4</h6>
+                  </div>
+                  <div style={{border:'2px solid #ccc',borderTop:'none'}}>
+                    <h6 style={{display:'flex',justifyContent:'center', alignItems:'center',margin:'5px 0'}}>News 5</h6>
+                  </div>
+                </div>
+              </div>
+            </div>
+        </div>
+        <div style={{display:'flex',justifyContent:'center', alignItems:'center'}}>
+          <img src='MinorProject-master\src\components\imgs\chhotkaImage.jpg' alt='imagesForScroll'/>
         </div>
       </div>
-      <div style={{display:'flex',justifyContent:'center', alignItems:'center'}}>
-        <img src='MinorProject-master\src\components\imgs\chhotkaImage.jpg' alt='image'/>
-      </div>
-    </div>
+    </>
   )
 }
 
--- src/components2/Section/ThirdPage.js
+++ src/components2/Section/ThirdPage.js
@@ -6,68 +6,68 @@ const ThirdPage = () => {
     <div style={{background:'#FAD2D2'}}>
       <Navbar3/>
       <div style={{marginTop:'40px',display:'flex', justifyContent:'center',alignItems:'center'}}>
-        <h1 style={{color:'#004080'}}>Your Path to Professional Excellence</h1>
+        <h1 style={{color:'#004080',fontSize:'3rem'}}>Your Path to Professional Excellence</h1>
       </div>
       <div style={{display:'flex', justifyContent:'space-evenly', alignItems:'center'}}>
         {/* left side */}
         <div>
             <div>
-                <img src='MinorProject-master\public\assets\chhotkaimage.jpg'/>
+                <img src='https://images.unsplash.com/photo-1486312338219-ce68d2c6f44d?w=450&h=250&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8dHlwaW5nJTIwbWFufGVufDB8fDB8fHww' alt='images1'/>
             </div>
             <div>
-                <img src='MinorProject-master\public\assets\chhotkaimage2.jpg'/>
+                <img src='https://images.unsplash.com/photo-1521790361543-f645cf042ec4?w=450&h=250&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTR8fHR5cGluZyUyMG1hbnxlbnwwfHwwfHx8MA%3D%3D' alt='images2'/>
             </div>
             <div>
-                <img src='MinorProject-master\public\assets\chhotkaimage3.jpg'/>
+                <img src='https://images.unsplash.com/photo-1531538606174-0f90ff5dce83?w=450&h=250&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjB8fHR5cGluZyUyMG1hbnxlbnwwfHwwfHx8MA%3D%3D' alt='images3'/>
             </div>
         </div>
         {/* right side */}
         <div>
             <div style={{background:"#5383FF",borderRadius:"5px",display:"flex",justifyContent:"center",alignItems:'center',marginTop:'30px',marginBottom:"-30px"}}>
-                <h3>"FORGE YOUR FUTURE: YOUR CAREER AWAITS."</h3>
+                <h3 style={{fontSize:'2rem'}}>"FORGE YOUR FUTURE: YOUR CAREER AWAITS."</h3>
             </div>
             <div style={{display:'flex',alignItems:'center',marginTop:'50px'}}>
                 <div style={{background:'#D2F2F2',height:'30vh',width:'15vw',padding:'10px'}}>
-                    <h5 style={{color:'#004080'}}>Resume Builder</h5>
+                    <h5 style={{color:'#004080',fontSize:'1.5rem'}}>Resume Builder</h5>
                     <ul>
                         <li>Access <br/> comprehensive <br/> lecture notes.</li>
                         <li>Contribute your <br/> own notes.</li>
                     </ul>
                 </div>
                 <div style={{background:'#A8B49A',height:'30vh',width:'15vw',padding:'10px'}}>
-                    <h5 style={{color:'#004080'}}>Career Path Resources</h5>
+                    <h5 style={{color:'#004080',fontSize:'1.5rem'}}>Career Path Resources</h5>
                     <ul>
                         <li>Engage with shared <br/> resources.</li>
                         <li>Discuss and<br/> collaborate with <br/> peers.</li>
                     </ul>
                 </div>
                 <div style={{background:'#D2F2F2',height:'30vh',width:'15vw',padding:'10px'}}>
-                    <h5 style={{color:'#004080'}}>Professional Development Courses</h5>
+                    <h5 style={{color:'#004080',fontSize:'1.5rem'}}>Professional Development Courses</h5>
                     <ul>
-                        <li>Refer to the  <br/> previous year's <br/> exam papers.</li>
+                        <li>Refer to the previous year's <br/> exam papers.</li>
                         <li>Contribute your <br/> own papers.</li>
                     </ul>
                 </div>
             </div>
             <div style={{display:'flex',alignItems:'center',marginTop:'50px'}}>
                 <div style={{background:'#D2F2F2',height:'30vh',width:'15vw',padding:'10px'}}>
-                    <h5 style={{color:'#004080'}}>Professional Toolkit</h5>
+                    <h5 style={{color:'#004080',fontSize:'1.5rem'}}>Professional Toolkit</h5>
                     <ul>
                         <li>Access <br/> comprehensive <br/> lecture notes.</li>
                         <li>Contribute your <br/> own notes.</li>
                     </ul>
                 </div>
                 <div style={{background:'#A8B49A',height:'30vh',width:'15vw',padding:'10px'}}>
-                    <h5 style={{color:'#004080'}}>Career Path Resources</h5>
+                    <h5 style={{color:'#004080',fontSize:'1.5rem'}}>Career Path Resources</h5>
                     <ul>
                         <li>Engage with shared <br/> resources.</li>
                         <li>Discuss and<br/> collaborate with <br/> peers.</li>
                     </ul>
                 </div>
                 <div style={{background:'#D2F2F2',height:'30vh',width:'15vw',padding:'10px'}}>
-                    <h5 style={{color:'#004080'}}>Professional Development Courses</h5>
+                    <h5 style={{color:'#004080',fontSize:'1.5rem'}}>Professional Development Courses</h5>
                     <ul>
-                        <li>Refer to the  <br/> previous year's <br/> exam papers.</li>
+                        <li>Refer to the previous year's <br/> exam papers.</li>
                         <li>Contribute your <br/> own papers.</li>
                     </ul>
                 </div>
